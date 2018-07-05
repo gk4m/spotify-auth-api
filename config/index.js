e708b0f888d995abe7ff5bc86bdcb84d3c0e91c3
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2018-07-05 18:10:32 +0200

    env detection

--- config/index.js
+++ config/index.js
@@ -1,3 +1,17 @@
+import local from './local';
+import prod from './prod';
+
+let config;
+
+switch (process.env.NODE_ENV) {
+  case 'local':
+    config = local;
+    break;
+
+  default:
+    config = prod;
+}
+
 export default {
-  port: 4040,
+  ...config
 }
