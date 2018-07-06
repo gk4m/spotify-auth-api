02ffe6d3edd2e920eaf7a7adf3c26852cc28c729
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2018-07-06 06:29:25 +0200

    update headers

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -15,8 +15,9 @@ export default function (app, express) {
 
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', "*");
+        res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
-        res.header('Access-Control-Allow-Headers', "Content-Type,x-access-token");
+        res.header('Access-Control-Allow-Headers', "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, x-access-token");
         next();
     });
 };
