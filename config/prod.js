32da9e36c41f72e5a84f33904fae4fb5f7afb56a
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2018-07-06 06:32:41 +0200

    update prod config

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,14 @@
 export default {
   port: 4041,
 
-  clientURL: '',
+  clientURL: 'https://gk4m.github.io/vue-spotify/',
 
   spotifyAPI: 'https://api.spotify.com/v1/',
   spotifyAccountsAPI: 'https://accounts.spotify.com/',
 
-  clientID: '',
-  clientSecret: '',
-  redirectUri: '',
+  clientID: '6eb45d91629c4f6c803a67f97a7f8fb1',
+  clientSecret: 'b24a6819d5b341dd9a967b3e3cfa28cc',
+  redirectUri: 'https://stark-harbor-25810.herokuapp.com/callback',
 
-  scope: ''
+  scope: 'user-read-private user-read-email user-read-birthdate user-read-playback-state user-modify-playback-state user-library-read playlist-read-private streaming'
 }
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -15,7 +15,6 @@ export default function (app, express) {
 
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', "*");
-        res.header("Access-Control-Allow-Credentials", true);
         res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE");
         res.header('Access-Control-Allow-Headers', "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, x-access-token");
         next();
